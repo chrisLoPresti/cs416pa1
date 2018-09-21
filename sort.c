a6d74ff150ba6615874aeab792930c79351f93b3
Chris LoPresti <lopresti.chris@yahoo.com>
2018-09-21 18:39:58 -0400

    removed notes

--- sort.c
+++ sort.c
@@ -6,23 +6,23 @@
 
 */
 
-#include <stdio.h> 
+#include <stdio.h>
 #include <string.h>
 #include "mapred.h"
 
-node * sort(node *head)
-{   
+node *sort(node *head)
+{
     printf("Performing lexicographical ordering\n");
     node *current;
     node *ptr;
-    for(current=head; current != NULL; current= current->next)
+    for (current = head; current != NULL; current = current->next)
     {
-        for(ptr=head; ptr != NULL; ptr= ptr->next)
+        for (ptr = head; ptr != NULL; ptr = ptr->next)
         {
-            if(strcmp(ptr->word,current->word) > 0)
+            if (strcmp(ptr->word, current->word) > 0)
             {
                 int count = current->count;
-                char * tempWord = current->word;
+                char *tempWord = current->word;
 
                 current->count = ptr->count;
                 current->word = ptr->word;
@@ -38,17 +38,17 @@ node * sort(node *head)
 
 void testSort()
 {
-    node * nodes;
+    node *nodes;
 
-    node * newNode1 = (node *)malloc(sizeof(node));
-    node * newNode2 = (node *)malloc(sizeof(node));
-    node * newNode3 = (node *)malloc(sizeof(node));
-    node * newNode4 = (node *)malloc(sizeof(node));
-    node * newNode5 = (node *)malloc(sizeof(node));
+    node *newNode1 = (node *)malloc(sizeof(node));
+    node *newNode2 = (node *)malloc(sizeof(node));
+    node *newNode3 = (node *)malloc(sizeof(node));
+    node *newNode4 = (node *)malloc(sizeof(node));
+    node *newNode5 = (node *)malloc(sizeof(node));
 
     newNode1->word = "a";
     newNode1->count = 1;
-    
+
     newNode2->word = "b";
     newNode2->count = 2;
 
@@ -68,21 +68,21 @@ void testSort()
     newNode5->next = NULL;
 
     nodes = newNode1;
-    node*temp = nodes;
+    node *temp = nodes;
 
     printf("printing unsorted link list: \n");
-    while (temp!=NULL)
+    while (temp != NULL)
     {
-        printf("%s,%d\n",temp->word,temp->count);
+        printf("%s,%d\n", temp->word, temp->count);
         temp = temp->next;
     }
 
-    node*head = sort(nodes);
+    node *head = sort(nodes);
 
     printf("Printing sorted link list: \n");
-    while (head!=NULL)
+    while (head != NULL)
     {
-        printf("%s,%d\n",head->word,head->count);
+        printf("%s,%d\n", head->word, head->count);
         head = head->next;
     }
 }
