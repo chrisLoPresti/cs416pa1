9d31caed70cfd0722884d074c5fa78222eaefc75
Chris LoPresti <lopresti.chris@yahoo.com>
2018-09-22 12:25:32 -0400

    threads no map to key pairs and store their results in a combined memory node list

--- threads.c
+++ threads.c
@@ -13,6 +13,7 @@ void initializeMemory(bucket *newBuckets, int newMapsOrThreads, int newReduces)
     head = newBuckets;
     mapsOrThreads = newMapsOrThreads;
     reduces = newReduces;
+    combinedData = NULL;
     produceThreads();
 }
 
@@ -23,34 +24,57 @@ void produceThreads()
         printf("\n mutex init failed\n");
         exit(EXIT_FAILURE);
     }
-    pthread_t mapperThread;
+    pthread_t mapperThread[mapsOrThreads];
     int i;
     for (i = 0; i < mapsOrThreads; i++)
     {
-        if (pthread_create(&mapperThread, NULL, map, (void *)head->keys) != 0)
+        if (pthread_create(&mapperThread[i], NULL, map, (void *)head->keys) != 0)
         {
             printf("Error creating thread\n");
             exit(EXIT_FAILURE);
         }
         head = head->next;
     }
-    pthread_join(mapperThread, NULL);
+    for (int i = 0; i < mapsOrThreads; i++)
+    {
+        pthread_join(mapperThread[i], NULL);
+    }
+
+    //not needed - just to print key pairs
+    while (combinedData != NULL)
+    {
+        printf("(%s,%d)\n", combinedData->word, combinedData->count);
+        combinedData = combinedData->next;
+    }
 }
 
 void *map(void *keys)
 {
     inputData *newKeys = keys;
-    node *keyPairs = (node *)malloc(sizeof(node));
-    node *keyPairsHead = keyPairs;
     while (newKeys != NULL)
     {
-        keyPairs->word = (char *)malloc(sizeof(char) * strlen(newKeys->word) + 1);
-        strcpy(keyPairs->word, newKeys->word);
-        keyPairs->word[strlen(keyPairs->word)] = '\0';
-        keyPairs->count = 1;
-        keyPairs->next = newKeys->next ? (node *)malloc(sizeof(node)) : NULL;
-        keyPairs = keyPairs->next;
+        pthread_mutex_lock(&dataLock);
+        if (combinedData == NULL)
+        {
+            combinedData = (node *)malloc(sizeof(node));
+            combinedData->word = (char *)malloc(sizeof(char) * strlen(newKeys->word) + 1);
+            strcpy(combinedData->word, newKeys->word);
+            combinedData->word[strlen(combinedData->word)] = '\0';
+            combinedData->count = 1;
+            combinedData->next = NULL;
+        }
+        else
+        {
+            node *temp = (node *)malloc(sizeof(node));
+            temp->word = (char *)malloc(sizeof(char) * strlen(newKeys->word) + 1);
+            strcpy(temp->word, newKeys->word);
+            temp->word[strlen(temp->word)] = '\0';
+            temp->count = 1;
+            temp->next = combinedData;
+            combinedData = temp;
+        }
         newKeys = newKeys->next;
+        pthread_mutex_unlock(&dataLock);
     }
     return NULL;
 }
