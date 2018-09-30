cb7045baa8c37fef74ef7385925d84e96bc96b7a
gdkat <gkatz97@gmail.com>
2018-09-30 11:35:16 -0400

    hard reset to master

---
+++ mapred.c
@@ -0,0 +1,237 @@
+#include "mapred.h"
+char *application;
+char *implementation;
+int numThreads;
+int reduces;
+int maps;
+int input;
+int output;
+node **buckets;
+int totalMapsOrExtra;
+int finalMapsOrExtra;
+int keyCount;
+
+//parese the input file and pull out words
+void parseInputFile()
+{
+    char *temp = (char *)calloc(1, 1);
+    char *singleChar = (char *)calloc(2, 1);
+
+    int itterator = 0;
+    while (read(input, &singleChar[0], 1) != 0)
+    {
+        if (validinput(singleChar[0]))
+        {
+            temp = realloc(temp, strlen(temp) + 2);
+            strcat(temp, singleChar);
+            continue;
+        }
+        if (strlen(temp) > 0)
+        {
+            char *p;
+            for (p = temp; *p; ++p)
+                *p = tolower(*p);
+            //add the words to our bucekts
+            buckets[itterator] = insertInput(buckets[itterator], temp);
+            free(temp);
+            temp = (char *)calloc(1, 1);
+            ++itterator;
+            if (itterator == totalMapsOrExtra)
+            {
+                itterator = 0;
+            }
+        }
+    }
+    if (strlen(temp) > 0)
+    {
+        char *p;
+        for (p = temp; *p; ++p)
+            *p = tolower(*p);
+        buckets[itterator] = insertInput(buckets[itterator], temp);
+    }
+    if (buckets[0] == NULL)
+    {
+        printf("The input file was empty, or there was an error reading from it\n");
+        exit(EXIT_FAILURE);
+    }
+    finalMapsOrExtra = keyCount < totalMapsOrExtra ? itterator + 1 : totalMapsOrExtra;
+    free(temp);
+    close(input);
+}
+
+//checks to make sure the character is not a delimiter
+int validinput(char character)
+{
+    if (character != ' ' && character != '\n' && character != '\t' && character != '\r' && character != '\0' && character != 0 && character != '.' && character != ',' && character != ';' && character != '!' && character != '-')
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+//adds a full word to our buckets
+node *insertInput(node *pointer, char *temp)
+{
+    node *tempData = (node *)malloc(sizeof(node));
+    tempData->word = (char *)malloc(sizeof(char) * strlen(temp) + 1);
+    strcpy(tempData->word, temp);
+    tempData->word[strlen(tempData->word)] = '\0';
+    tempData->next = pointer;
+    pointer = tempData;
+    tempData->count = 0;
+    ++keyCount;
+    return pointer;
+}
+
+//creates our buckets based off of the number of requested maps
+void initializeBuckets()
+{
+    totalMapsOrExtra = maps ? maps : numThreads;
+
+    buckets = (node **)malloc(sizeof(node *) * totalMapsOrExtra);
+    int i;
+    for (i = 0; i < totalMapsOrExtra; ++i)
+    {
+        buckets[i] = malloc(sizeof(node));
+        buckets[i] = NULL;
+    }
+}
+
+//removes any buckets that were not used
+void *cleanBuckets()
+{
+    int i;
+    for (i = finalMapsOrExtra; i < totalMapsOrExtra; ++i)
+    {
+        free(buckets[i]);
+    }
+    return NULL;
+}
+
+//drives the program
+int main(int argc, char **argv)
+{
+    // if we do not have 6 or 7 inputs we can not continue
+    if (argc != 7 && argc != 6)
+    {
+        printf("Please make sure to have 6 or 7 inputs\n 7: ./mapred –-app [-wordcount, -sort] –-impl [-procs, -threads] --maps -[num_maps] –-reduces -[num_reduces] --input [-infile] –-output [-outfile]\n  6: ./mapred –-app [-wordcount, -sort] –-impl [-extra] --numthreads -[num] --input [-infile] –-output [-outfile]\n");
+        exit(EXIT_FAILURE);
+    }
+
+    //before doing anything lets check if these files names exist/ are taken
+    input = argc == 7 ? open(++argv[5], O_RDONLY) : open(++argv[4], O_RDONLY);
+    //if we couldn"t open the file let the user know it doesnt exist
+    if (input < 0)
+    {
+        close(input);
+        printf("We were unable to open your input file. Please check to make sure that file exists, or confirm the spelling.\n Please use the form -[file name]\n");
+        exit(EXIT_FAILURE);
+    }
+    //lets create the output file
+    output = argc == 7 ? open(++argv[6], O_RDWR | O_CREAT, 0777) : open(++argv[5], O_RDWR | O_CREAT, 0777);
+    //if we could not create the file let them know and exit(EXIT_FAILURE)
+    if (output < 0)
+    {
+        printf("We were unable to open your output file. Either there was an error creating it, or it already exists and we do not have permission to open it.\n Please use the form -[file name]\n");
+        exit(EXIT_FAILURE);
+    }
+
+    //get the application type (wordcount or sort)
+    application = (char *)(malloc(sizeof(char) * strlen(argv[1]) + 1));
+    strcpy(application, argv[1]);
+    application[strlen(application)] = '\0';
+    //make sure the application type is valid
+    if (strcmp(application, "-wordcount") != 0 && strcmp(application, "-sort") != 0)
+    {
+        printf("You entered an invalid --app.\nValid options=>\n-wordcount\n-sort\nPlease try again with a valid option.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    //get the implementation method(procs, threads, extra)
+    implementation = (char *)(malloc(sizeof(char) * strlen(argv[2]) + 1));
+    strcpy(implementation, argv[2]);
+    implementation[strlen(implementation)] = '\0';
+    //make sure the implementation type is valid
+    if (strcmp(implementation, "-procs") != 0 && strcmp(implementation, "-threads") != 0 && strcmp(implementation, "-extra") != 0)
+    {
+        printf("You entered an invalid --impl.\nValid options=>\n-procs\n-threads\n-extra\nPlease try again with a valid option.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    //validate to make sure we have the correct input amount matching our implementation
+    if (argc == 7 && (strcmp(implementation, "-procs") == 0 || strcmp(implementation, "-threads") == 0))
+    {
+        //lets get the number of maps
+        maps = atoi(++argv[3]);
+        //make sure it is a valid number
+        if (maps <= 0)
+        {
+            printf("You entered an invalid --maps value of <=0 or some form of characters.\n Please use the form -[num]\n");
+            exit(EXIT_FAILURE);
+        }
+
+        //lets get the number of reduces
+        reduces = atoi(++argv[4]);
+        //make sure it is a valid number
+        if (reduces <= 0)
+        {
+            printf("You entered an invalid --numReduces value of <=0 or some form of characters.\n Please use the form -[num]\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+    else if (argc == 6 && strcmp(implementation, "-extra") == 0)
+    {
+        // lets get the number of threads
+        numThreads = atoi(++argv[3]);
+        //make sure it is a valid number
+        if (numThreads <= 0)
+        {
+            printf("You entered an invalid --num_threads value of <=0 or some form of characters.\n Please use the form -[num]\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+    //if the input stream is invalid let them know
+    else
+    {
+        printf("Please make sure to have 6 or 7 inputs\n 7: ./mapred –-app [-wordcount, -sort] –-impl [-procs, -threads] --maps -[num_maps] –-reduces -[num_reduces] --input [-infile] –-output [-outfile]\n  6: ./mapred –-app [-wordcount, -sort] –-impl [-extra] --numthreads -[num] --input [-infile] –-output [-outfile]\n");
+        exit(EXIT_FAILURE);
+    }
+
+    //create our buckets
+    initializeBuckets();
+    //read the input file and fill the buckets
+    parseInputFile();
+    //create a thread to clean the buckets we did not use
+
+    pthread_t cleaner;
+    if (pthread_create(&cleaner, NULL, cleanBuckets, NULL) != 0)
+    {
+        printf("Error creating thread\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (strcmp(implementation, "-threads") == 0)
+    {
+        threading_driver(buckets, finalMapsOrExtra, reduces, output, application, keyCount);
+    }
+    pthread_join(cleaner, NULL);
+    return 0;
+}
+
+/* 
+this is code you can use to test to see if the buckets have information
+
+    printf("(we need %d, buckets and will remove any excess)\n", finalMapsOrExtra);
+    int i = 0;
+    for (i = 0; i < finalMapsOrExtra; i++)
+    {
+        while (buckets[i] != NULL)
+        {
+            printf("(%s,%d)\n", buckets[i]->word, buckets[i]->count);
+            buckets[i] = buckets[i]->next;
+        }
+    }
+    */
---
+++ threads.c
@@ -0,0 +1,350 @@
+#include "threads.h"
+
+node **buckets; // buckets used in reduce are circular and point to last node
+node *dataLinkList;
+pthread_mutex_t dataLock;
+int mapsOrThreads;
+int reduces;
+int outputFile;
+int totalNodes;
+char *app;
+int *reduceCountArray; // this int array is used to store how many nodes are needed for each bucket in the reduce stage
+
+void threading_driver(node **newBuckets, int newMapsOrThreads, int newReduces, int output, char *application, int keyCount)
+{
+    // initialize all information
+    {
+        buckets = newBuckets;
+        mapsOrThreads = newMapsOrThreads;
+        reduces = newReduces;
+        outputFile = output;
+        totalNodes = keyCount;
+        app = malloc(strlen(application) + 1);
+        strcpy(app, application);
+    }
+    // setup mutex lock
+    initializeDataLinkListMutexLock();
+    // create threads and run mapping function
+    produceThreadMapsAndWaitTillAllThreadsFinish();
+    // sort data
+    dataLinkList = sort(dataLinkList, app);
+    // printLinkList();
+    generateHowManyNodesEachBucketWillContain();
+    // printReduceCountArray();
+    configureBucketsToContainCorrectNumberOfNodes();
+    // // printBuckets();
+    produceReduceThreadsAndWaitTillAllThreadsFinish();
+    // printBuckets();
+    finalReduce();
+    // printBuckets();
+    writeToFile();
+}
+
+void initializeDataLinkListMutexLock()
+{
+    if (pthread_mutex_init(&dataLock, NULL) != 0)
+    {
+        printf("\n mutex init failed\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void produceThreadMapsAndWaitTillAllThreadsFinish()
+{
+    pthread_t mapperThread[mapsOrThreads];
+    int i;
+    for (i = 0; i < mapsOrThreads; i++)
+    {
+        if (buckets[i] && pthread_create(&mapperThread[i], NULL, map, (void *)buckets[i]) != 0)
+        {
+            printf("Error creating thread\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    for (i = 0; i < mapsOrThreads; i++)
+    {
+        pthread_join(mapperThread[i], NULL);
+    }
+}
+
+void *map(void *keys)
+{
+    node *bucketData = (node *)keys;
+    node *startptr = bucketData;
+    node *lastptr;
+    while (bucketData != NULL)
+    {
+        bucketData->count = 1;
+        lastptr = bucketData;
+        bucketData = bucketData->next;
+    }
+
+    pthread_mutex_lock(&dataLock);
+
+    if (!dataLinkList)
+    {
+        dataLinkList = startptr;
+    }
+    else
+    {
+        lastptr->next = dataLinkList;
+        dataLinkList = startptr;
+    }
+    pthread_mutex_unlock(&dataLock);
+
+    return NULL;
+}
+
+void printLinkList()
+{
+    printf("Starting printing link list\n");
+    node *tmp = dataLinkList;
+    int x = 0;
+    while (tmp != NULL)
+    {
+        printf("%s, %d\n", tmp->word, tmp->count);
+        tmp = tmp->next;
+        x++;
+    }
+    printf("total num of words: %d\n", x);
+    printf("completed printing link list\n");
+}
+
+void generateHowManyNodesEachBucketWillContain()
+{
+    // printf("generating each bucket number\n");
+    // create array which will store how many nodes each reduce thread will handle
+    reduceCountArray = calloc(reduces + 1, sizeof(int));
+    reduceCountArray[reduces] = '\0';
+
+    int x;
+    int reduceCountArrayIndex = 0;
+
+    for (x = 0; x < totalNodes; x++)
+    {
+        if (reduceCountArrayIndex == reduces)
+        {
+            reduceCountArrayIndex = 0;
+        }
+        reduceCountArray[reduceCountArrayIndex] = reduceCountArray[reduceCountArrayIndex] + 1;
+        reduceCountArrayIndex++;
+    }
+    // printf("finished generating each bucket number\n");
+}
+
+void printReduceCountArray()
+{
+    printf("Starting printing array of num\n");
+    int x = 0;
+    while (x < reduces)
+    {
+        printf("%d\n", reduceCountArray[x]);
+        x++;
+    }
+    printf("completed printing array of num\n");
+}
+
+void configureBucketsToContainCorrectNumberOfNodes()
+{
+    // printf("starting configuring buckets\n");
+    int x;
+    for (x = 0; x < reduces; x++)
+    {
+        if (reduceCountArray[x] == '\0')
+        {
+            break;
+        }
+        node *head = dataLinkList;
+        node *tmp = dataLinkList;
+        int y;
+
+        for (y = 0; y < reduceCountArray[x] - 1; y++) // minus one because error was caused without it
+        {
+            tmp = tmp->next;
+        }
+        // move main pointer to next starting position
+        dataLinkList = tmp->next;
+
+        // create circular link list in bucket
+        buckets[x] = head;
+        tmp->next = buckets[x];
+        buckets[x] = tmp;
+    }
+    // printf("finished configuring buckets\n");
+}
+
+void printBuckets()
+{
+    printf("Starting printing buckets\n");
+    int x;
+    for (x = 0; x < reduces; x++)
+    {
+        printf("Printing data in bucket %d:\n", x);
+        if (buckets[x] == NULL)
+        {
+            break;
+        }
+        node *tmp = buckets[x]->next;
+        while (tmp != buckets[x])
+        {
+            printf("%s, %d\n", tmp->word, tmp->count);
+            tmp = tmp->next;
+        }
+        printf("%s, %d\n", tmp->word, tmp->count);
+    }
+    printf("Ending printing buckets\n");
+}
+
+void produceReduceThreadsAndWaitTillAllThreadsFinish()
+{
+    // printf("tarting reduce threading\n");
+    pthread_t reduceThread[reduces];
+    int i;
+    for (i = 0; i < reduces; i++)
+    {
+        reduceCountArray[i] = i;
+        // not sure why you need (size_t)
+        if (buckets[i] && pthread_create(&reduceThread[i], NULL, reduce, (void *)(size_t)reduceCountArray[i]) != 0)
+        {
+            printf("Error creating thread\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    for (i = 0; i < reduces; i++)
+    {
+        pthread_join(reduceThread[i], NULL);
+    }
+    // printf("finished reduce threading\n");
+}
+
+void *reduce(void *bucketNumber)
+{
+    node *tail = buckets[(int)bucketNumber];
+    node *head = tail->next;
+    tail->next = NULL; // changes input list from circular to linear
+    node *results = NULL;
+    node *resultsPtr = NULL;
+
+    while (head != NULL)
+    {
+        if (resultsPtr == NULL)
+        {
+            results = head;
+            resultsPtr = head;
+            head = head->next;
+        }
+        else
+        {
+            if (strcmp(resultsPtr->word, head->word) == 0)
+            {
+                resultsPtr->count = resultsPtr->count + head->count;
+                node *tmp = head->next;
+                free(head->word);
+                free(head);
+                head = tmp;
+            }
+            else
+            {
+                resultsPtr->next = head;
+                resultsPtr = resultsPtr->next;
+                head = head->next;
+            }
+        }
+    }
+
+    resultsPtr->next = results;
+    buckets[(int)bucketNumber] = resultsPtr;
+    return NULL;
+}
+
+void finalReduce()
+{
+    // printf("starting final reduce\n");
+    int x;
+    for (x = 0; x < reduces; x++)
+    {
+        if (buckets[x + 1] != NULL && buckets[x] != NULL)
+        {
+            // if the last node in the bucket x has the same words as the first node in bucket x+1 then need to combine
+            if (strcmp(buckets[x]->word, buckets[x + 1]->next->word) == 0)
+            {
+                buckets[x]->count = buckets[x]->count + buckets[x + 1]->next->count;
+                // check if only one node in bucket
+                if (buckets[x + 1]->next == buckets[x + 1])
+                {
+                    buckets[x + 1] = NULL;
+                    moveBucketsToTheLeft(x + 1); // need to move all buckets over 1 to the left
+                }
+                else
+                {
+                    buckets[x + 1]->next = buckets[x + 1]->next->next;
+                }
+                // reset index to 0 but since for loop will add +1 at the end of the loop must set x -1
+                x = -1;
+            }
+        }
+    }
+    // printf("finished final reduce\n");
+}
+
+void moveBucketsToTheLeft(int startingBucket)
+{
+    // printf("starting shifting buckets over\n");
+    int x;
+    for (x = startingBucket; x < reduces - 1; x++)
+    {
+        buckets[x] = buckets[x + 1];
+    }
+    buckets[reduces - 1] = NULL;
+    // printf("finished shifting buckets over\n");
+}
+
+void writeToFile()
+{
+    int x;
+    for (x = 0; x < reduces; x++)
+    {
+        if (buckets[x] == NULL)
+        {
+            break;
+        }
+
+        node *temp = buckets[x]->next;
+
+        while (temp != buckets[x])
+        {
+            processWrite(temp);
+            temp = temp->next;
+        }
+        processWrite(temp);
+    }
+}
+
+void processWrite(node *temp)
+{
+    char buf[1000];
+    if (write(outputFile, temp->word, strlen(temp->word)) < 0)
+    {
+        printf("Error writing to the file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (write(outputFile, " ", 1) < 0)
+    {
+        printf("Error writing to the file\n");
+        exit(EXIT_FAILURE);
+    }
+    sprintf(buf, "%d", temp->count);
+    if (write(outputFile, buf, strlen(buf)) < 0)
+    {
+        printf("Error writing to the file\n");
+        exit(EXIT_FAILURE);
+    }
+    if (write(outputFile, "\n", 1) < 0)
+    {
+        printf("Error writing to the file\n");
+        exit(EXIT_FAILURE);
+    }
+}
